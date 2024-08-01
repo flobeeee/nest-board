c4b1762bc1ce18da333e9f17053848d96aef0f3f
ejlee <sssver28@gmail.com>
2024-08-01 13:40:50 +0900

    get, post api 생성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,11 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
-  constructor(private boardsService: BoardsService) {
-    
-    this.boardsService.getAllboards() // 이렇게 바로 사용 가능
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards(); // 이렇게 바로 사용 가능
   }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string 
+  ): Board {
+    return this.boardsService.createBoard(title, description);
+  }
+
+  /* post 요청시 받은 응답
+    {
+    "id": "46e90980-4fbf-11ef-b074-0561d4eb0c3a",
+    "title": "Board 1",
+    "description": "Description",
+    "status": "PUBLIC"
+  }
+  */
 }
- 
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid} from 'uuid';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = []; // 우선 디비와 연동 전에 로직먼저 생성하기 위해 
+
+  getAllBoards(): Board[] {
+    return this.boards
+  }
+
+  // id에 유니크값을 넣기 위해 uuid 라이브러리 설치 진행
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC
+    }
+
+    this.boards.push(board);
+    return board;
+  }
+}
